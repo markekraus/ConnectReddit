097880fc7ee2d96c28b17a423845bf0c7f134a3b
Mark Kraus <git@markekraus.com>
2016-10-11 16:50:53 -0500

    Confirm-RedditOAuthAccessTokenResponse fixes 1.0.0.36
    Throw was missing a few messages. Also tests turned up an uncaught error when the content is not a JSON object.

--- Connect-Reddit.psd1
+++ Connect-Reddit.psd1
@@ -4,7 +4,7 @@
 	ModuleToProcess = 'Connect-Reddit.psm1'
 	
 	# Version number of this module.
-	ModuleVersion = '1.0.0.35'
+	ModuleVersion = '1.0.0.36'
 	
 	# ID used to uniquely identify this module
 	GUID = 'a1fb1d76-b5c9-4880-9f1c-56d47986e459'
--- OAuth/Confirm-RedditOAuthAccessTokenResponse.ps1
+++ OAuth/Confirm-RedditOAuthAccessTokenResponse.ps1
@@ -45,7 +45,12 @@ function Confirm-RedditOAuthAccessTokenResponse {
         }
         Write-Verbose 'Client credentials ok.'
         Write-Verbose 'Converting Content to Object'
-        $TokenObject = $Response.Content | ConvertFrom-Json
+        try {
+            $TokenObject = $Response.Content | ConvertFrom-Json -ErrorAction Stop
+        }
+        catch {
+            Throw 'Response does not contain valid JSON Object'
+        }
         Write-Verbose 'Checking if error present'
         if ($TokenObject.error) {
             Write-Output $false
@@ -55,13 +60,14 @@ function Confirm-RedditOAuthAccessTokenResponse {
             }
             else {
                 $Message = "Uknknown error code: {0}" -f $TokenObject.error
-                Throw
+                Throw $Message
             }
         }
         Write-Verbose 'No error found.'
         Write-Verbose 'Verifying Access Token was retruned.'
         if (!$TokenObject.access_token) {
             $Message = "access_token not found. response: {0}" -f $($TokenObject | ConvertTo-Json -Compress)
+            Throw $Message
         }
         Write-verbose 'Access Token found.'
         Write-Output $True
