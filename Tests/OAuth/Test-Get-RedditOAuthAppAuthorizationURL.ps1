e9808f3d7c4989d9773164801edbeaa64586568a
Mark Kraus <git@markekraus.com>
2016-10-13 16:32:20 -0500

    More Tests

---
+++ Tests/OAuth/Test-Get-RedditOAuthAppAuthorizationURL.ps1
@@ -0,0 +1,30 @@
+﻿$TestName = Split-Path -Path $PSCommandPath -Leaf
+$TestsRequired = @('Test-New-RedditApplication.ps1')
+foreach ($TestRequired in $TestsRequired) {
+    if ($TestRequired -notin $Global:TestsCompleted) {
+        $RequiredTestScript = Get-ChildItem -Recurse -Path ..\ -Filter $TestRequired
+        Write-Host "Running tests from '$($RequiredTestScript.FullName)'"
+        . $RequiredTestScript.FullName
+    }
+}
+
+InModuleScope 'Connect-Reddit' {
+    Describe 'Get-RedditOAuthAppAuthorizationURL' {
+        It 'Returns a Code authorization URL' {
+            $Params = @{
+                Application = $Global:RedditAppWeb
+                State = [System.Guid]::new('86263983-387d-4d6a-abdb-4965677ad281')
+                
+            }
+            $Url = Get-RedditOAuthAppAuthorizationURL @Params
+            $Url | Should Match 'client_id=12345'
+            $Url | Should Match 'response_type=code'
+            $Url | Should Match 'state=86263983-387d-4d6a-abdb-4965677ad281'
+            $Url | Should Match 'scope=account%2c'
+            $Url | Should Match 'redirect_uri=http%3a%2f%2f127.0.0.1%3a65010%2fauthorize_callback'
+            $Url | Should Match 'duration=permanent'
+        }
+    }
+}
+
+$Global:TestsCompleted += $TestName
---
+++ Tests/OAuth/Test-Get-RedditOAuthAuthorizationHeader.ps1
@@ -0,0 +1,35 @@
+﻿$TestName = Split-Path -Path $PSCommandPath -Leaf
+$TestsRequired = @('Test-New-RedditOAuthAccessToken.ps1')
+foreach ($TestRequired in $TestsRequired) {
+    if ($TestRequired -notin $Global:TestsCompleted) {
+        $RequiredTestScript = Get-ChildItem -Recurse -Path ..\ -Filter $TestRequired
+        Write-Host "Running tests from '$($RequiredTestScript.FullName)'"
+        . $RequiredTestScript.FullName
+    }
+}
+
+InModuleScope 'Connect-Reddit' {
+    Describe 'Get-RedditOAuthAuthorizationHeader' {
+        It 'Returns an Auhtorzation header for a plain text user/password' {
+            $Param = @{
+                UserName = 'User'
+                Password = 'Password'
+            }
+            Get-RedditOAuthAuthorizationHeader @Param | Should Be 'Basic VXNlcjpQYXNzd29yZA=='
+        }
+        It 'Returns an Authorization header for an application'{
+            $Param = @{
+                Application = $Global:RedditAppWeb
+            }
+            Get-RedditOAuthAuthorizationHeader @Param | Should Be 'Basic MTIzNDU6NTQzMjE='
+        }
+        It 'Returns an Authorization header for an Access Token' {
+            $Param = @{
+                AccessToken = $Global:RedditTokenWeb
+            }
+            Get-RedditOAuthAuthorizationHeader @Param | Should Be 'Basic MTIzNDU6NTQzMjE='
+        }
+    }
+}
+
+$Global:TestsCompleted += $TestName
